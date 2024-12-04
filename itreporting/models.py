5e7a10254f3f46fa8c99bcea26afe72ca2f85d4a
dchambers120 <c1052778@hallam.shu.ac.uk>
2024-12-04 12:11:17 +0000

    1

--- itreporting/models.py
+++ itreporting/models.py
@@ -11,7 +11,7 @@ class Issue(models.Model):
     details = models.TextField()
     date_submitted = models.DateTimeField(default=timezone.now)
     description = models.TextField()
-    author = models.ForeignKey(User, related_name = 'issue',
+    author = models.ForeignKey(User, related_name = 'issues',
     on_delete=models.CASCADE)
     def __str__(self):
 
